81cf86041a7449bf925962f34a3cd5435e28586b
nsjethani <neha.jethani@sjsu.edu>
2018-02-18 01:41:48 +0000

    mlab

--- app.js
+++ app.js
@@ -54,11 +54,11 @@ var db_pwd  = "cmpe281" ;
 var db_name = "test" ;
 */
 
-var db_host = (process.env.mongodb_host || "localhost" ) ;
-var db_port = (process.env.mongodb_port || "27017" ) ;
+var db_host = (process.env.mongodb_host || "ds139645.mlab.com" ) ;
+var db_port = (process.env.mongodb_port || "39645" ) ;
 var db_user = (process.env.mongodb_user || "cmpe281" ) ;
 var db_pwd  = (process.env.mongodb_pwd  || "cmpe281" ) ;
-var db_name = (process.env.mongodb_name || "test" ) ;
+var db_name = (process.env.mongodb_name || "cmpe281" ) ;
    
 
 var db = new DB(db_name,
